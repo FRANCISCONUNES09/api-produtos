3de3ade3d705a5730140d402921030e59a07ae1c
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-06-06 11:57:25 -0300

    Back-end

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const port = 4057;
 require('./src/models')
 const produtosRoutes = require('./src/routes/produtos')
 const usersRoutes = require('./src/routes/users')
+const authRoutes = require('./src/routes/auth')
 
 app.use(express.json())
 app.use(produtosRoutes)
 app.use(usersRoutes)
+app.use(authRoutes)
 
 app.listen(port, () => {
   console.log(`O servidor está rodando na porta ${port}`)
